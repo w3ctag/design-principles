2c090d5e763e73fbea11529ee3bf3dbcf9b1f5f5
L. David Baron <dbaron@dbaron.org>
2017-04-28 18:33:59 +0900

    Revert debugging.
    It showed:
    remote: Invalid username or password.
    fatal: Authentication failed for ...

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -12,4 +12,4 @@ git config user.name "Travis-CI"
 git config user.email "travis@w3ctag.org"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
